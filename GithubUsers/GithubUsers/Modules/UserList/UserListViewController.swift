b1b4acc8e4b704670fa756d461e5a58d18b47113
Failyn Kaye Sedik <failynsedik@gmail.com>
2021-11-10 03:24:47 +0800

    Add initial network call implementation for testing purposes

--- GithubUsers/GithubUsers/Modules/UserList/UserListViewController.swift
+++ GithubUsers/GithubUsers/Modules/UserList/UserListViewController.swift
@@ -8,19 +8,31 @@
 import UIKit
 
 class UserListViewController: UIViewController {
+    // MARK: - Initializer
+
+    private let viewModel: UserListViewModel
+
+    init(viewModel: UserListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        viewModel.getUsers()
+            .done { _ in
+                print("✅ Call Successful")
+            }
+            .catch { _ in
+                print("❌ Call Failed")
+            }
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-     }
-     */
 }
